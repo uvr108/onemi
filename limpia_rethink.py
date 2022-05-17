034c773816f5f6923505fb54fd9bc5536bce29f8
Ulises <ulises108@gmail.com>
2022-05-16 21:20:20 -0400

    Se apunto a localhost

--- limpia_rethink.py
+++ limpia_rethink.py
@@ -1,7 +1,7 @@
 from rethinkdb import RethinkDB
 
 r = RethinkDB()
-rethink_r = r.connect(host='10.54.223.19', port=28015, db='csn').repl()
+rethink_r = r.connect(host='127.0.0.1', port=28015, db='csn').repl()
 
 
 res = r.table('ULI_SOFT').delete().run()
