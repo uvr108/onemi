c0cdac959606cee042e0de6cd1666b99550a6f48
uvergara <uvergara@controlnet.lan.csn.uchile.cl>
2022-05-20 12:28:02 -0400

    Commit incial

---
+++ graficos.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import glob
+from obspy.core import read
+from obspy.core import UTCDateTime
+import configparser
+from pathlib import Path
+import os
+
+
+def generagraph(net,stat):
+
+    ahora = UTCDateTime()
+    timestamp = UTCDateTime().timestamp
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    # userinfo = config["USERINFO"]
+    serverinfo = config["DIRECTORIES"]
+
+    yr = ahora.year
+    jl = "%03d" % ahora.julday
+
+    image = f'{timestamp}.png'
+
+    pat = f"*{yr}.{jl}"
+    st = read(f"mnt/data/{yr}/{net}/{stat}/HHE.D/{net}*{pat}")
+    st += read(f"mnt/data/{yr}/{net}/{stat}/HHN.D/{net}*{pat}")
+    st += read(f"mnt/data/{yr}/{net}/{stat}/HHZ.D/{net}*{pat}")
+    tr = st[0]
+    dt = st[0].stats.starttime
+
+   
+    ruta = f'{serverinfo["onemi"]}/formas/sismos/{net}/{stat}/{jl}'
+
+    print(ruta)
+
+    if Path(ruta).is_dir():
+        pass 
+        print('Si existe', serverinfo["sismos"]) 
+    else:
+        os.makedirs(ruta, exist_ok=True) 
+    st.plot(outfile=f'{ruta}/{image}', starttime=ahora-310, endtime=ahora-10) 
+    return {'image': image , 'jl': jl}
+
+def main():
+
+    print(generagraph('CX','PB06'))
+
+if __name__ == "__main__":
+
+    main()
+
+
---
+++ mount.sh
@@ -0,0 +1,2 @@
+sudo mount -t nfs 10.54.217.11:/active/seiscomp/archive  /home/uvergara/Projectos/Python/onemi/mnt/data
+
